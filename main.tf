d6edad86f2287df9ede44cd9be36f3279d1739fc
aqeebrasool-del <aqeebrasool@gmail.com>
2026-07-04 14:44:06 +0530

    Update main.tf
    rg3 added from feature2

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "rg1"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg3"
+  location = "West Europe"
+}
